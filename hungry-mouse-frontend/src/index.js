6f0e8f4c9cd11e98d2588fbf0684a8dad4bdc99d
Pat <patrick.s.santucci@gmail.com>
2019-06-17 17:22:02 +0100

    table updates

--- hungry-mouse-frontend/src/index.js
+++ hungry-mouse-frontend/src/index.js
@@ -1,22 +1,60 @@
-const GRID_URL = "http://localhost:3000/grid";
+const BASE_URL = "http://localhost:3000";
+const GRID_URL = `${BASE_URL}/grid`;
+const FOOD_URL = `${BASE_URL}/food`;
 const gameBoard = document.querySelector("#game-grid");
+const body = document.querySelector("body");
 
 function fetchGrid() {
   fetch(GRID_URL)
     .then(resp => resp.json())
-    .then(resp => generateRows(resp[0]));
+    .then(generateRows);
 }
 
 function generateRows(gridArray) {
-  debugger;
   for (let y = 0; y < gridArray.length; y++) {
     const tr = document.createElement("tr");
-    tr.setAttribute("id", `r${y + 1}`);
+    tr.id = `r${y + 1}`;
     gameBoard.appendChild(tr);
     for (let x = 0; x < gridArray[y].length; x++) {
       const td = document.createElement("td");
-      td.setAttribute("id", `r${y + 1}c${x + 1}`);
+      td.dataset.gridLocation = `[${y + 1}][${x + 1}]`;
+      td.height = "22px";
+      td.width = "22px";
+      // td.addEventListener("click", () =>
+      //   alert(`You sunk my battleship! ${td.dataset.gridLocation}`)
+      // );
       tr.appendChild(td);
     }
   }
+
+  const start = document.createElement("button");
+  start.id = "start-button";
+  start.textContent = "Start game";
+  start.addEventListener("click", startGame);
+  body.appendChild(start);
 }
+
+function startGame(e) {
+  e.preventDefault();
+  fetchFood();
+}
+
+function fetchFood() {
+  fetch(FOOD_URL)
+    .then(resp => resp.json())
+    .then(foodArray => foodArray.forEach(food => placeFood(food)));
+}
+
+function placeFood(food) {
+  const length = food.length;
+
+  alert(`Please select where you'd like to place your ${food.name}`);
+}
+
+function placementGrid() {}
+
+function init() {
+  fetchGrid();
+}
+
+init();
